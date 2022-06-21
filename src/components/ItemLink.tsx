c29599e8b33748f0baa74e7491e55e6284e7c7ae
Rupak-66 <rbhattacharjee26@yahoo.com>
2022-06-21 12:32:52 +0530

    Updated Prompt logic

--- src/components/ItemLink.tsx
+++ src/components/ItemLink.tsx
@@ -1,15 +1,24 @@
-import { NavLink} from "react-router-dom";
+import { NavLink, Prompt } from "react-router-dom";
 import * as React from "react";
 
 const ItemLink: React.FC<{ product: string }> = (props) => {
+  const [click, setClick] = React.useState<boolean>(true);
+
+  const onClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    setClick(true);
+    console.log(event.type === "click");
+   
+    console.log(click);
+  };
+
 
   return (
     <td>
-      <NavLink to={`/product/${props.product}`} exact >
+      <NavLink to={`/product/${props.product}`} exact onClick={onClick}>
         {props.product}
-      </NavLink> 
+      </NavLink>
       {/* <button  onClick={onClick}>{props.product}</button> */}
-      
+      <Prompt key={props.product} when={click} message="test ?" />;
     </td>
   );
 };
