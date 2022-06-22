7e7c6b438ab79cd1edcaf9646054af87be171728
Rupak-66 <rbhattacharjee26@yahoo.com>
2022-06-22 11:13:42 +0530

    Add POST call logic and some css

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -13,14 +13,20 @@ type UserSubmitForm = {
 const schema = yup.object().shape({
   product: yup
     .string()
-    .min(3,"* Product must have 3 characters")
-    .required("Product is required value & should have 3 characters"),
+    .min(3, "* Product must have 3 characters")
+    .required("* Product is required value"),
   quantity: yup
     .number()
-    .positive()
-    .integer()
-    .required("Quantity should be more than 0"),
-  price: yup.number().positive().required("Price should be more than 1"),
+    .typeError("* Quantity field can't be NULL")
+    .positive("* Quantity should be a positive number")
+    .integer("* Quantity should be integer only")
+    .required("* Quantity should be more than 0"),
+  price: yup
+    .number()
+    .typeError("* Price field can't be NULL")
+    .min(1, "* Price should be minimum 1")
+    .positive("* Price should be a positive number")
+    .required("* Price should be more than 1"),
 });
 
 const AddProduct: React.FC = () => {
@@ -32,30 +38,31 @@ const AddProduct: React.FC = () => {
     resolver: yupResolver(schema),
   });
 
-  const submitForm = (data: UserSubmitForm) => {};
+  const submitForm = async (data: UserSubmitForm) => {
+    const {price, quantity, product: product_name} = data;
+    const response = await fetch("http://localhost:3004/products",{
+      method: 'POST',
+      body: JSON.stringify({price, quantity, product_name}),
+      headers:{
+        "Content-Type": "application/json"
+      }
+    });
+    const productData = await response.json();
+    console.log(productData);
+  };
 
   return (
     <div className="product-form">
       <h1>Add Product</h1>
       <form onSubmit={handleSubmit(submitForm)}>
-
-        <input type="text" {...register("product")} placeholder="product" />
-        
-        <p style={{color:'red'}}>{errors.product?.message}</p>
-        <input
-          type="number"
-          placeholder="quantity"
-          {...register("quantity")}
-        />
-        <p>{errors.quantity?.message}</p>
-        <input
-          type="number"
-          {...register("price")}
-          placeholder="price"
-        />
-        <p>{errors.price?.message}</p>
+        <input type="text" {...register("product")} placeholder="Product" />
+        <p style={{ color: "red" }}>{errors.product?.message}</p>
+        <input type="number" placeholder="Quantity" {...register("quantity")} />
+        <p style={{ color: "red" }}>{errors.quantity?.message}</p>
+        <input type="number" {...register("price")} placeholder="Price" />
+        <p style={{ color: "red" }}>{errors.price?.message}</p>
         <button className="form-btn" type="submit">
-          Submit
+          SUBMIT
         </button>
       </form>
     </div>
