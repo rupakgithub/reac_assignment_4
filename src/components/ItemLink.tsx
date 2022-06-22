1d32bb0014e36650e63feda296863009173638cf
Rupak-66 <rbhattacharjee26@yahoo.com>
2022-06-22 11:21:19 +0530

    Added minor code fix

--- src/components/ItemLink.tsx
+++ src/components/ItemLink.tsx
@@ -8,13 +8,13 @@ const ItemLink: React.FC<{ product: string }> = (props) => {
      
       <Prompt
         message={(location, action) => {
-          if (action === "POP") {
-            console.log("Backing up...");
-          }
+          // if (action === "POP") {
+          //   console.log("Backing up...");
+          // }
 
           return !location.pathname.startsWith("/product/")
             ? true
-            : `Are you sure you want to go to ${location.pathname}?`;
+            : "Are you sure you want to view the details ?";
         }}
       />
       <td>
